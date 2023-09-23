70b48e95be74733c93b86cbfc44004676ebd8caa
Bersen Naidoo <bersen.naidoo@gmail.com>
2023-09-23 12:34:21 +0200

    implemented logging package with zerolog

--- cmd/lib/main.go
+++ cmd/lib/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/bersennaidoo/lib/pkg/infrastructure/config"
+	"github.com/bersennaidoo/lib/pkg/infrastructure/logger"
 )
 
 func main() {
@@ -12,6 +13,17 @@ func main() {
 	if err != nil {
 		fmt.Errorf("%w", err)
 	}
+
+	trace := logger.TRACE
+
+	logcfg := logger.LogConfig{
+		Environment: cfg.Environment,
+		LogLevel:    trace,
+	}
+
+	logsrv := logger.NewZeroLoggerSrv(logcfg)
+
+	logsrv.Info().Msg("Hello from zeroLog")
 
 	fmt.Printf("%s %s %s %s %s %s\n", cfg.Rpc.Host, cfg.Rpc.Port, cfg.Environment, cfg.PG.Conn,
 		cfg.Web.Port, cfg.Web.Host)
